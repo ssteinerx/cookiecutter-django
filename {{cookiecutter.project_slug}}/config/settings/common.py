5fab5eb1062726a428fdddef179c5314ac568664
Steve Steiner <ssteinerX@gmail.com>
2016-09-15 12:25:29 -0400

    Revert "Merge branch 'wip-migrate-to-django-1.10' of https://github.com/pydanny/cookiecutter-django"
    This reverts commit a967cceda95bd7ca168ebcbb26827e47922a9242, reversing
    changes made to a42eff9f1f429924adee933951ead4456ba8f15d.

--- {{cookiecutter.project_slug}}/config/settings/common.py
+++ {{cookiecutter.project_slug}}/config/settings/common.py
@@ -53,10 +53,9 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 # MIDDLEWARE CONFIGURATION
 # ------------------------------------------------------------------------------
-MIDDLEWARE = (
+MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -200,26 +199,6 @@ ROOT_URLCONF = 'config.urls'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
 WSGI_APPLICATION = 'config.wsgi.application'
-
-
-# PASSWORD VALIDATION
-# https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
-# ------------------------------------------------------------------------------
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
 
 # AUTHENTICATION CONFIGURATION
 # ------------------------------------------------------------------------------
